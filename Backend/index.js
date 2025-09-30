bf6a99dc8caf9ab66a477e6548ffb8ce390f8a12
Sahil Dhingani <sahildhingani51@gmail.com>
2025-09-30 10:33:13 +0530

    1

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: [
     "http://localhost:5173",         // local dev
-    "https://funiture-psi.vercel.app/Shop"    // deployed frontend
+    "https://funiture-psi.vercel.app"    // deployed frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
